0b2b3620e8fe99e237baf642208bb5f5d1b7253f
annikaul <annika-kristin.kaul1@informatik.hs-fulda.de>
2024-10-29 17:54:36 +0100

    #4 - Add routes for about pages and assets

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -3,6 +3,37 @@ import { homeController } from "../controllers";
 
 const router = Router();
 
+const frontendPath = __dirname + '/../../../../frontend'
+const assetPath = __dirname + '/../../../../frontend/assets'
+
+
 router.get("/", homeController);
+
+// Assets
+router.get('/assets/:assetname', function (req, res) {
+    res.sendFile(req.params.assetname, {
+        root: assetPath
+    });
+});
+
+// About Page
+router.get('/about', function (req, res) {
+    res.sendFile('about.html', {
+        root: frontendPath
+    });
+});
+
+// Single about pages
+router.get('/about/:aboutmember', function (req, res) {
+    res.sendFile(req.params.aboutmember, {
+        root: frontendPath
+    });
+});
+
+router.get('/style.css', function (req, res) {
+    res.sendFile('style.css', {
+        root: frontendPath
+    });
+});
 
 export default router;
