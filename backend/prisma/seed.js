9c99b1420a396cc56bb4e8df75e27a83a6df91c0
Kemjiga <kemerjg@gmail.com>
2024-11-26 11:46:03 +0100

    #39: update seed script and fix typo

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -11,16 +11,16 @@ async function main() {
   // Populate the database
   console.log("Seeding database...");
   const listings = [
-    { name: 'Apartment A', postcode: '10001', apartmentType: 'single apartment', rent: 1200.5 },
-    { name: 'Apartment B', postcode: '10002', apartmentType: 'shared apartment', rent: 750.0 },
-    { name: 'Apartment C', postcode: '10003', apartmentType: 'sublet', rent: 950.0 },
-    { name: 'Apartment D', postcode: '10004', apartmentType: 'single apartment', rent: 1400.0 },
-    { name: 'Apartment E', postcode: '10005', apartmentType: 'shared apartment', rent: 800.0 },
-    { name: 'Apartment F', postcode: '10006', apartmentType: 'sublet', rent: 1000.0 },
-    { name: 'Apartment G', postcode: '10007', apartmentType: 'single apartment', rent: 1250.0 },
-    { name: 'Apartment H', postcode: '10008', apartmentType: 'shared apartment', rent: 850.0 },
-    { name: 'Apartment I', postcode: '10009', apartmentType: 'sublet', rent: 1100.0 },
-    { name: 'Apartment J', postcode: '10010', apartmentType: 'single apartment', rent: 1300.0 },
+    { name: 'Apartment A', postcode: '10001', apartment_type: 'single apartment', rent: 1200.5 },
+    { name: 'Apartment B', postcode: '10002', apartment_type: 'shared apartment', rent: 750.0 },
+    { name: 'Apartment C', postcode: '10003', apartment_type: 'sublet', rent: 950.0 },
+    { name: 'Apartment D', postcode: '10004', apartment_type: 'single apartment', rent: 1400.0 },
+    { name: 'Apartment E', postcode: '10005', apartment_type: 'shared apartment', rent: 800.0 },
+    { name: 'Apartment F', postcode: '10006', apartment_type: 'sublet', rent: 1000.0 },
+    { name: 'Apartment G', postcode: '10007', apartment_type: 'single apartment', rent: 1250.0 },
+    { name: 'Apartment H', postcode: '10008', apartment_type: 'shared apartment', rent: 850.0 },
+    { name: 'Apartment I', postcode: '10009', apartment_type: 'sublet', rent: 1100.0 },
+    { name: 'Apartment J', postcode: '10010', apartment_type: 'single apartment', rent: 1300.0 },
   ];
 
   for (const listing of listings) {
