23317505a3e22d7299bb4af6e05ae401fed67410
Kemjiga <kemerjg@gmail.com>
2024-11-27 16:46:52 +0100

    #54: add cors policy and morgan for logs

--- backend/main.js
+++ backend/main.js
@@ -2,6 +2,7 @@ require("dotenv-flow").config();
 
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const prisma = require("./src/utils/db");
 const router = require("./src/routes");
 
@@ -9,7 +10,20 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Allow requests from the frontend
-app.use(cors({ origin: "http://localhost:4173" }));
+const allowedOrigins = ['https://findful.us.to'];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
+app.use(morgan("dev"));
 
 // helps in validating a successful database connection
 async function startServer() {
