9e5fa75189ec6e46562ac07aa41ac6c1340da75d
Kemjiga <kemerjg@gmail.com>
2024-12-04 22:03:45 +0100

    #66: fix typo on header component name

--- frontend/findful/src/pages/Homepage.jsx
+++ frontend/findful/src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { getAllListings, searchListing } from "../services/listingService";
 import "./Homepage.css";
-import Header from "../components/header/Header";
+import FHeader from "../components/header/Header";
 import Map from "../components/map/Map";
 import { Input, Select, Button, Row, Col, Card, Form } from "antd";
 import Meta from "antd/es/card/Meta";
@@ -27,7 +27,7 @@ const Homepage = () => {
 
   return (
     <div className="homepage">
-      <Header />
+      <FHeader />
 
       <div className="content">
         <h1>Search for Apartments</h1>
