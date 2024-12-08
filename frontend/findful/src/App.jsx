740076bcd52177d0e5454b5d28eaad68362f0f69
Kemjiga <kemerjg@gmail.com>
2024-12-08 20:56:38 +0100

    #58: add login and register routes

--- frontend/findful/src/App.jsx
+++ frontend/findful/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import React from "react";
 
 const App = () => {
@@ -9,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
