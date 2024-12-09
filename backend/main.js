7841773892f38dd5e2d3dfb870c796d7786e6b33
Kemjiga <kemerjg@gmail.com>
2024-12-09 15:28:54 +0100

    #58: add cors credentials

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,5 @@
 require("dotenv-flow").config();
-
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
@@ -21,6 +21,7 @@ const allowedOrigins = [
 // const allowedOrigins = ["http://localhost:5174/"];
 
 const corsOptions = {
+  credentials: true,
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
@@ -32,6 +33,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 const server = http.createServer(app);
 const io = new Server(server, {
