7f1910933618d82828f687dea0bcdd399bfaf992
sarah-gdsd <sarah.majeed@informatik.hs-fulda.de>
2024-12-19 11:23:45 +0100

    add: Chat and message routing and frontend connection for getUserChats

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const listingRouter = require("./listingRouter");
 const userRouter = require("./userRouter");
+const chatRouter = require("./chatRouter");
+const messageRouter = require("./messageRouter");
 
 const router = express.Router();
 
 // Use the listing routes
 router.use("/listings", listingRouter);
 router.use("/users", userRouter);
+
+// message routes
+router.use("/chats", chatRouter);
+router.use("/messages", messageRouter);
 
 //Add other routes here. Eg: userRouter
 router.get("/", (req, res) => {
