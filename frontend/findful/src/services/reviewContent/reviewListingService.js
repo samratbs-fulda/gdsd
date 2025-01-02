693293527b5fa73d304038c595549c29f2c4270f
Kemjiga <kemerjg@gmail.com>
2025-01-02 20:20:52 +0100

    #66: fix endpoint route typo

--- frontend/findful/src/services/reviewContent/reviewListingService.js
+++ frontend/findful/src/services/reviewContent/reviewListingService.js
@@ -5,13 +5,8 @@ const environment = getEnvironment();
 const apiUrl = environment.VITE_BACKEND;
 
 export const getReviewListings = async (status) => {
-  const payload = {
-    params: {
-      status: status,
-    },
-  };
   try {
-    const response = await axios.get(`${apiUrl}/api/listings/review`, payload);
+    const response = await axios.get(`${apiUrl}/api/listings/review/${status}`);
     return response.data.listings;
   } catch (error) {
     console.error("Failed to fetch users:", error);
