3fda1391624949771390bae5577d576829e71069
fdai6619 <annika-kristin.kaul1@informatik.hs-fulda.de>
2025-01-08 18:39:55 +0100

    No Ticket - Change highlight color, font and header height

--- frontend/findful/src/App.jsx
+++ frontend/findful/src/App.jsx
@@ -14,55 +14,68 @@ import FindFulHeader from "./components/header/FindFulHeader";
 import FindFulFooter from "./components/footer/FindFulFooter";
 import AddListing from "./pages/AddListing/AddListing";
 import { AuthContext } from './services/authContext';
+import Paragraph from "antd/es/typography/Paragraph";
+import useBreakpoint from "antd/lib/grid/hooks/useBreakpoint";
 
 const App = () => {
-  const { token } = theme.useToken();
   const { user } = React.useContext(AuthContext);
+
+  const breakpoint = useBreakpoint();
+
   return (
     <div className="app">
+      {/* Custom color theme */}
       <ConfigProvider
         theme={{
-          // Customization possible here
+          token: {
+            colorPrimary: '#75B541', // HS Fulda green
+            colorLink: '#75B541',
+
+            fontFamily: 'Lato',
+          },
           components: {
             Typography: {
               fontSizeHeading1: 40,
+            },
+            Layout: {
+              headerBg: "#fff",
             }
           }
         }}
       >
 
-      <Layout className='main-layout'>
+        <Layout className='main-layout'>
           <Paragraph className="project-notice" type="secondary" italic={true} style={{ marginBottom: 0 }}>Fulda University of Applied Sciences Software Engineering Project, Fall 2024 For Demonstration Only</Paragraph>
-        <Header><FindFulHeader /></Header>
+          <Header style={{ height: breakpoint.xl ? "8vh" : breakpoint.sm ? "10vh" : "12vh" }}><FindFulHeader /></Header>
 
-        <Content className='route-content'>
-          <Routes>
-            {/* Public routes */}
-            <Route path="/" element={<Homepage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+          <Content className='route-content'>
+            <Routes>
+              {/* Public routes */}
+              <Route path="/" element={<Homepage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
 
-            {/* Protected routes to moderator */}
-            <Route path="/dashboard" element={
-              <ProtectedRoute requiredRole="MODERATOR" user={user}>
-                <Dashboard />
-              </ProtectedRoute>
-            } />
-            
-            {/* Protected routes to landlord */}
-            <Route path="/listing/add" element={
-              <ProtectedRoute requiredRole="LANDLORD" user={user}>
-                <AddListing />
-              </ProtectedRoute>
-            } />
+              {/* Protected routes to moderator */}
+              <Route path="/dashboard" element={
+                <ProtectedRoute requiredRole="MODERATOR" user={user}>
+                  <Dashboard />
+                </ProtectedRoute>
+              } />
 
-            <Route path="/chat" element={<Chat />} />
-            <Route path="/listing/:id" element={<ListingDetailsPage />} />
-          </Routes>
-        </Content>
+              {/* Protected routes to landlord */}
+              <Route path="/listing/add" element={
+                <ProtectedRoute requiredRole="LANDLORD" user={user}>
+                  <AddListing />
+                </ProtectedRoute>
+              } />
 
-        <Footer><FindFulFooter /></Footer>
-      </Layout>
+              <Route path="/chat" element={<Chat />} />
+              <Route path="/listing/:id" element={<ListingDetailsPage />} />
+            </Routes>
+          </Content>
+
+          <Footer><FindFulFooter /></Footer>
+        </Layout>
       </ConfigProvider>
     </div>
   );
