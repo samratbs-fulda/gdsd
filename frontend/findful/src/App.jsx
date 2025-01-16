8d79f56ce5a7832d24763ad657b2e131f745d9af
ARZuhaib <abdul-rahman.zuhaib@informatik.hs-fulda.de>
2025-01-16 10:14:59 +0100

    #60: Merge conflict fixes

--- frontend/findful/src/App.jsx
+++ frontend/findful/src/App.jsx
@@ -16,6 +16,8 @@ import AddListing from "./pages/AddListing/AddListing";
 import { AuthContext } from './services/authContext';
 import Paragraph from "antd/es/typography/Paragraph";
 import useBreakpoint from "antd/lib/grid/hooks/useBreakpoint";
+import EditProfilePage from './pages/profile/EditProfilePage';
+
 
 const App = () => {
   const { user } = React.useContext(AuthContext);
@@ -39,9 +41,6 @@ const App = () => {
             },
             Layout: {
               headerBg: "#fff",
-            },
-            Carousel: {
-              arrowSize: 32,
             }
           }
         }}
@@ -74,6 +73,7 @@ const App = () => {
 
               <Route path="/chat" element={<Chat />} />
               <Route path="/listing/:id" element={<ListingDetailsPage />} />
+              <Route path="/profile" element={<EditProfilePage />} />
             </Routes>
           </Content>
 
