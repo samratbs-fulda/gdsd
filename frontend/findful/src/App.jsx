e7f4c48122f73121dbad26e61fe94076653641de
ARZuhaib <abdul-rahman.zuhaib@informatik.hs-fulda.de>
2025-01-16 12:37:28 +0100

    #60: Minor fix

--- frontend/findful/src/App.jsx
+++ frontend/findful/src/App.jsx
@@ -41,6 +41,9 @@ const App = () => {
             },
             Layout: {
               headerBg: "#fff",
+            },
+            Carousel: {
+              arrowSize: 32,
             }
           }
         }}
