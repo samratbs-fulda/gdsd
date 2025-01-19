8c95b8b618893382956fb56477d1408d05db3111
Kemjiga <kemerjg@gmail.com>
2025-01-19 23:00:22 +0100

    #83: Add status filter

--- frontend/findful/src/services/landlord/listingsByLandlord.js
+++ frontend/findful/src/services/landlord/listingsByLandlord.js
@@ -4,14 +4,15 @@ import { getEnvironment } from "../../utils/fetchEnvironment";
 const environment = getEnvironment();
 const apiUrl = environment.VITE_BACKEND;
 
-export const getListingsByLandlordId = async (landlordId) => {
+export const getListingsByLandlordId = async (landlordId, status) => {
   const payload = {
     params: {
       landlordId: landlordId,
+      status: status,
     },
   };
   try {
-    const response = await axios.get(`${apiUrl}/api/listings/review`, payload);
+    const response = await axios.get(`${apiUrl}/api/listings/review/landlord`, payload);
     return response.data.listings;
   } catch (error) {
     console.error("Failed to fetch users:", error);
